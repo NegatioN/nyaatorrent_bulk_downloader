56f522d6e83b567d4716f3777b5162cb34e3df5a
Joakim Rishaug <Joakimrishaug@gmail.com>
2015-03-04 23:45:34 +0100

    added os.sep for compat in paths, reset working dir after download.

--- nyaatorrent_downloader/downloader.py
+++ nyaatorrent_downloader/downloader.py
@@ -7,15 +7,19 @@ def download_series(series):
     #TODO implement setting for where you wanna save torrents
     series_name = series.getName()
     path = createFolder(series_name, "torrents")
+    oldPath = os.getcwd()       #used to get back to cwd
     os.chdir(path)      #changes current working directory to curDir/torrents/series-name
     for torrent in series.getTorrents():
         download_torrent(torrent.getName(), torrent.getUrl())
+
+    #reset working directory to what we had before saving torrents
+    os.chdir(oldPath)
 
 
 #Downloads a single torrent file.
 def download_torrent(title, url):
     print('Downloading >> ' + title)
-    fname = os.getcwd() + '/' + title + '.torrent'
+    fname = os.getcwd() + os.sep + title + '.torrent'
     # http://stackoverflow.com/a/14114741/1302018
     try:
         r = requests.get(url, stream=True)
@@ -43,7 +47,7 @@ def getContents(url):
 #creates a folder within current working dir
 #returns path of dir
 def createFolder(name, path): #name of folder, and a default sub-folder you want to add
-    path=os.getcwd() + '\\' + path + '\\' + name
+    path=os.getcwd() + os.sep + path + os.sep + name
     if not os.path.isdir(path):
         os.makedirs(path)
     return path
